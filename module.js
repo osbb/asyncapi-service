3e0691189bdd9560bd27327d7232c0df24cb84b9
Dmytro Les <dmitriy.s.les@gmail.com>
2016-09-09 17:24:41 +0300

    init

--- module.js
+++ module.js
@@ -3,7 +3,11 @@
 import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
-import { MongoClient, ObjectId } from 'mongodb';
+import amqp from 'amqplib';
+import uuid from 'uuid';
+
+const rabbitmqHost = process.env.RABBITMQ_PORT_5672_TCP_ADDR || 'localhost';
+const rabbitmqPort = process.env.RABBITMQ_PORT_5672_TCP_PORT || 5672;
 
 const app = express();
 const server = http.createServer(app);
@@ -13,39 +17,54 @@ server.listen(3001);
 
 io.set('origins', '*:*');
 
-const mongoHost = process.env.MONGO_PORT_27017_TCP_ADDR || 'localhost';
-const mongoPort = process.env.MONGO_PORT_27017_TCP_PORT || 27017;
-const connection = MongoClient.connect(`mongodb://${mongoHost}:${mongoPort}`); // connection promise
-const decisionsCollectionPromise = connection.then(db => db.collection('decisions'));
+const amqpChannelPromise = amqp.connect(`amqp://${rabbitmqHost}:${rabbitmqPort}`)
+  .then(conn => conn.createChannel());
 
-function loadDecisions(socket) {
-  decisionsCollectionPromise
-    .then(c => c.find({}).toArray())
-    .then(data => socket.emit('LOAD_DECISIONS', data));
-}
-
-function updateDecision(socket, decision) {
-  const { title, answer } = decision;
-
-  decisionsCollectionPromise
-    .then(c => c.updateOne({ _id: ObjectId(decision._id) }, { $set: { title, answer } }))
-    .then(() => decisionsCollectionPromise
-      .then(c => c.findOne({ _id: ObjectId(decision._id) }, {})))
-    .then(data => socket.emit('UPDATE_DECISION', data));
-}
-
-function createDecision(socket, decision) {
-  const { title, answer } = decision;
-
-  decisionsCollectionPromise
-    .then(c => c.insertOne({ title, answer }, {}))
-    .then(res => decisionsCollectionPromise
-      .then(c => c.findOne({ _id: ObjectId(res.insertedId) }, {})))
-    .then(data => socket.emit('CREATE_DECISION', data));
-}
+const corrToSock = {}; // correlation ID to socket ID map
 
 io.on('connection', socket => {
-  socket.on('LOAD_DECISIONS', () => loadDecisions(socket));
-  socket.on('UPDATE_DECISION', decision => updateDecision(socket, decision));
-  socket.on('CREATE_DECISION', decision => createDecision(socket, decision));
+  amqpChannelPromise
+    .then(ch => {
+      ch.assertQueue('', { exclusive: true })
+        .then(q => {
+          ch.consume(q.queue, msg => {
+            const socketId = corrToSock[msg.properties.correlationId];
+            const { type, data } = JSON.parse(msg.content.toString());
+            io.to(socketId).emit(type, data);
+          }, { noAck: true });
+
+          socket.on('LOAD_DECISIONS', () => {
+            const correlationId = uuid.v4();
+
+            ch.sendToQueue(
+              'events',
+              new Buffer(JSON.stringify({ type: 'LOAD_DECISIONS' })),
+              { persistent: true, correlationId, replyTo: q.queue }
+            );
+            corrToSock[correlationId] = socket.id;
+          });
+
+          socket.on('UPDATE_DECISION', decision => {
+            const correlationId = uuid.v4();
+
+            ch.sendToQueue(
+              'events',
+              new Buffer(JSON.stringify({ type: 'UPDATE_DECISION', decision })),
+              { persistent: true, correlationId, replyTo: q.queue }
+            );
+            corrToSock[correlationId] = socket.id;
+          });
+
+          socket.on('CREATE_DECISION', decision => {
+            const correlationId = uuid.v4();
+
+            ch.sendToQueue(
+              'events',
+              new Buffer(JSON.stringify({ type: 'CREATE_DECISION', decision })),
+              { persistent: true, correlationId, replyTo: q.queue }
+            );
+            corrToSock[correlationId] = socket.id;
+          });
+        });
+    });
 });
