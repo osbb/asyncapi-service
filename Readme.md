94bd6c78835702f6f38c488979ad4a5dcb6d0851
Dmytro Les <dmitriy.s.les@gmail.com>
2016-09-15 15:58:26 +0300

    topics

--- Readme.md
+++ Readme.md
@@ -5,6 +5,6 @@ docker build -t osbb/asyncapi-service . --no-cache
 
 # Run
 ```
-docker run -d --name mongo mongo
+docker run -d --hostname rabbitmq --name rabbitmq rabbitmq:3
 docker run -d -p 3001:3001 --link mongo --name asyncapi osbb/asyncapi-service
 ```
--- module.js
+++ module.js
@@ -12,55 +12,80 @@ const rabbitmqPort = process.env.RABBITMQ_PORT_5672_TCP_PORT || 5672;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-
-server.listen(3001);
-
-io.set('origins', '*:*');
-
-const amqpChannelPromise = amqp.connect(`amqp://${rabbitmqHost}:${rabbitmqPort}`)
-  .then(conn => conn.createChannel());
-
+const nodeId = uuid.v4();
 const corrToSock = {}; // correlation ID to socket ID map
 
+server.listen(3001);
+io.set('origins', '*:*');
+
 io.on('connection', socket => {
-  amqpChannelPromise
+  amqp.connect(`amqp://${rabbitmqHost}:${rabbitmqPort}`)
+    .then(conn => conn.createChannel())
     .then(ch => {
-      ch.assertQueue('', { exclusive: true })
+      ch.assertQueue(`decisions.load.listener.${nodeId}`, { exclusive: true })
         .then(q => {
           ch.consume(q.queue, msg => {
             const socketId = corrToSock[msg.properties.correlationId];
-            const { type, data } = JSON.parse(msg.content.toString());
-            io.to(socketId).emit(type, data);
+            const data = JSON.parse(msg.content.toString());
+            io.to(socketId).emit('LOAD_DECISIONS', data);
           }, { noAck: true });
 
           socket.on('LOAD_DECISIONS', () => {
             const correlationId = uuid.v4();
 
-            ch.sendToQueue(
+            ch.assertExchange('events', 'topic', { durable: true });
+            ch.publish(
               'events',
-              new Buffer(JSON.stringify({ type: 'LOAD_DECISIONS' })),
-              { persistent: true, correlationId, replyTo: q.queue }
+              'decisions.load',
+              new Buffer(JSON.stringify(null)),
+              {
+                persistent: true,
+                correlationId,
+                replyTo: q.queue,
+              }
             );
             corrToSock[correlationId] = socket.id;
           });
+        });
+
+      ch.assertQueue(`decisions.update.listener.${nodeId}`, { exclusive: true })
+        .then(q => {
+          ch.consume(q.queue, msg => {
+            const socketId = corrToSock[msg.properties.correlationId];
+            const data = JSON.parse(msg.content.toString());
+            io.to(socketId).emit('UPDATE_DECISION', data);
+          }, { noAck: true });
 
           socket.on('UPDATE_DECISION', decision => {
             const correlationId = uuid.v4();
 
-            ch.sendToQueue(
+            ch.assertExchange('events', 'topic', { durable: true });
+            ch.publish(
               'events',
-              new Buffer(JSON.stringify({ type: 'UPDATE_DECISION', decision })),
+              'decisions.update',
+              new Buffer(JSON.stringify(decision)),
               { persistent: true, correlationId, replyTo: q.queue }
             );
             corrToSock[correlationId] = socket.id;
           });
+        });
+
+      ch.assertQueue(`decisions.create.listener.${nodeId}`, { exclusive: true })
+        .then(q => {
+          ch.consume(q.queue, msg => {
+            const socketId = corrToSock[msg.properties.correlationId];
+            const data = JSON.parse(msg.content.toString());
+            io.to(socketId).emit('CREATE_DECISION', data);
+          }, { noAck: true });
 
           socket.on('CREATE_DECISION', decision => {
             const correlationId = uuid.v4();
 
-            ch.sendToQueue(
+            ch.assertExchange('events', 'topic', { durable: true });
+            ch.publish(
               'events',
-              new Buffer(JSON.stringify({ type: 'CREATE_DECISION', decision })),
+              'decisions.create',
+              new Buffer(JSON.stringify(decision)),
               { persistent: true, correlationId, replyTo: q.queue }
             );
             corrToSock[correlationId] = socket.id;
